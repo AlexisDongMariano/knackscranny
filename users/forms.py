4f69889c2d1c10647839759045a9b93622ad08a8
AlexisDongMariano <popfansin@gmail.com>
2020-10-03 02:34:44 +0800

    updated profile template and form

--- users/forms.py
+++ users/forms.py
@@ -25,7 +25,21 @@ class UserRegisterForm(UserCreationForm):
 class CustomerUpdateForm(forms.ModelForm):
     # widgets = {'about_me': forms.Textarea(attrs={'cols': 80})}
     
+
     class Meta:
         model = Customer
         fields = ['first_name', 'last_name', 'email', 'contact1', 'contact2', 'image']
+        widgets = {
+            'first_name': forms.TextInput(attrs={'class': 'form-control', 'required': True}),
+            'last_name': forms.TextInput(attrs={'class': 'form-control', 'required': True}),
+            'email': forms.EmailInput(attrs={'class': 'form-control', 'required': True}),
+            'contact1': forms.NumberInput(attrs={'class': 'form-control', 'required': True}),
+            'contact2': forms.NumberInput(attrs={'class': 'form-control'}),
+            # 'image': forms.FileInput(attrs={'class': 'form-control-file clearablefileinput', 'accept': 'image/*'})
+            'image': forms.FileInput(attrs={'class': 'custom-file-input', 'accept': 'image/*'})
+        }
+
+# class CustomerUpdateForm(forms.Form):
+#     first_name = forms.CharField(required=True)
+
         
