4a96bf08e439a07074ab612f120bcb3f6b92c978
AlexisDongMariano <popfansin@gmail.com>
2020-10-16 02:29:20 +0800

    updated login/logout links and redirections

--- knackscranny/urls.py
+++ knackscranny/urls.py
@@ -12,6 +12,7 @@ urlpatterns = [
     path('payment/', include('payment.urls')),
     # path('register/', user_views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html', redirect_authenticated_user=True), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page=settings.LOGOUT_REDIRECT_URL), name='logout')
 ]
 
 if settings.DEBUG:
